7a35f0664f8eeda38483b68276e3bf2bc738a3cd
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2021-08-26 19:53:00 -0700

    update

---
+++ codewars/filter_list.js
@@ -0,0 +1,13 @@
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    
+    let newArr = [];
+    for(let i = 0; i < l.length; i++) {
+        if(typeof l[i] == 'number') {
+        newArr.push(l[i]);
+        }
+    }
+    return newArr;
+}
+
+console.log(filter_list([1,2,'a','b']))
